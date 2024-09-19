0bae9adada74eadeaa3f3bf29612e6249a29c196
Christopher Gallant <thraxus@gmail.com>
2024-09-19 09:05:16 -0300

    clean up unnecessary usings

--- Source/Dynamic_Carrying_Capacity_Patch/MassUtility_Capacity_Patch.cs
+++ Source/Dynamic_Carrying_Capacity_Patch/MassUtility_Capacity_Patch.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Text;
 using HarmonyLib;
 using RimWorld;
 using Verse;
